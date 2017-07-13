1eca5132241a115a1e8cd828416790f3bc9cb76f
administrator <liquidnuker@users.noreply.github.com>
2017-07-13 14:37:06 -0700

    initial commit

---
+++ src/vue-components/CheckToggle1.vue
@@ -0,0 +1,36 @@
+<template>
+  <div>
+    <div class="row col-sm-12">    
+      <!-- checkToggle1-01 -->
+      <template v-for="i in items">
+      <span class="checkToggle1-01">
+        <input type="checkbox" v-bind:id="i"
+        v-model="selectedItems" v-bind:value="i" />
+        <label v-bind:for="i" class="checkToggle1-01_bg">
+          <span class="checkToggle1-01_switch">
+            &nbsp;
+          </span>
+        </label>
+        <label v-bind:for="i" class="checkToggle1-01_vlabel">
+          {{ i }}
+        </label>
+      </span>
+      </template>
+      <!-- end checkToggle1-01 -->
+      <p>Selected: {{ selectedItems }}</p>
+    </div>  
+  </div>
+</template>
+<script>
+export default {
+  data () {
+    return {
+      items: [
+        "item1",
+        "item2"
+      ],
+      selectedItems: []
+    }
+  }
+}
+</script>
---
+++ src/vue-components/Home.vue
@@ -0,0 +1,30 @@
+<template>
+  <div>
+    <h2 class="item-id">vueCheckToggle1-01</h2>
+    <!-- vueCheckToggle1-01 mount-->
+    <div id="vcCheckToggle1_mount"></div>
+    <!-- end vueCheckToggle1-01 mount-->
+  </div>
+</template>
+<script>
+import {inject} from "../js/componentinjector.js";;
+  export default {
+    data () {
+      return {    
+      }
+    },
+    mounted: function () {
+      this.mountCheckToggle1();
+    },
+    methods: {
+      mountCheckToggle1: function() {
+        const CheckToggle1 = resolve => {
+          require.ensure(['./CheckToggle1.vue'], () => {
+            resolve(require('./CheckToggle1.vue'))
+          })
+        };
+        inject("#vcCheckToggle1_mount", CheckToggle1);
+      }
+    }
+  }
+</script>
