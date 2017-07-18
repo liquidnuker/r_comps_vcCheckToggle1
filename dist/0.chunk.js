a267e977db6b6e88ce5b574c814eb8b8d2e02429
administrator <liquidnuker@users.noreply.github.com>
2017-07-18 09:38:41 -0700

    changed Home.vue layout

--- dist/0.chunk.js
+++ dist/0.chunk.js
@@ -1,1 +1,151 @@
-webpackJsonp([0],{10:function(e,n,t){"use strict";t.d(n,"a",function(){return u});var u=function(e,n){new Vue({el:e,render:function(e){return e(n)}})}},13:function(e,n){e.exports={render:function(){var e=this,n=e.$createElement;e._self._c;return e._m(0)},staticRenderFns:[function(){var e=this,n=e.$createElement,t=e._self._c||n;return t("div",[t("h2",{staticClass:"item-id"},[e._v("vueCheckToggle1-01")]),e._v(" "),t("div",{attrs:{id:"vcCheckToggle1_mount"}})])}]}},7:function(e,n,t){var u=t(0)(t(9),t(13),null,null);e.exports=u.exports},9:function(e,n,t){"use strict";Object.defineProperty(n,"__esModule",{value:!0});var u=t(10);n.default={data:function(){return{}},mounted:function(){this.mountCheckToggle1()},methods:{mountCheckToggle1:function(){var e=function(e){t.e(1).then(function(){e(t(8))}.bind(null,t)).catch(t.oe)};u.a("#vcCheckToggle1_mount",e)}}}}});
+webpackJsonp([0],{
+
+/***/ 10:
+/***/ (function(module, __webpack_exports__, __webpack_require__) {
+
+"use strict";
+/* harmony export (binding) */ __webpack_require__.d(__webpack_exports__, "a", function() { return inject; });
+// for injecting components in mounts
+var inject = function inject(mount, component) {
+  new Vue({
+    el: mount,
+    render: function render(h) {
+      return h(component);
+    }
+  });
+};
+
+
+
+/***/ }),
+
+/***/ 13:
+/***/ (function(module, exports, __webpack_require__) {
+
+module.exports={render:function (){var _vm=this;var _h=_vm.$createElement;var _c=_vm._self._c||_h;
+  return _vm._m(0)
+},staticRenderFns: [function (){var _vm=this;var _h=_vm.$createElement;var _c=_vm._self._c||_h;
+  return _c('div', [_c('h2', {
+    staticClass: "item-id"
+  }, [_vm._v("vueCheckToggle1-01")]), _vm._v(" "), _c('div', {
+    staticClass: "row"
+  }, [_c('div', {
+    staticClass: "col-sm-2"
+  }, [_c('div', {
+    attrs: {
+      "id": "vcCheckToggle1_mount"
+    }
+  })]), _vm._v(" "), _c('div', {
+    staticClass: "col-sm-2"
+  }, [_vm._v("\n      2\n    ")]), _vm._v(" "), _c('div', {
+    staticClass: "col-sm-2"
+  }, [_vm._v("\n      3\n    ")]), _vm._v(" "), _c('div', {
+    staticClass: "col-sm-2"
+  }, [_vm._v("\n      4\n    ")]), _vm._v(" "), _c('div', {
+    staticClass: "col-sm-2"
+  }, [_vm._v("\n      5\n    ")]), _vm._v(" "), _c('div', {
+    staticClass: "col-sm-2"
+  }, [_vm._v("\n      6\n    ")])])])
+}]}
+module.exports.render._withStripped = true
+if (false) {
+  module.hot.accept()
+  if (module.hot.data) {
+     require("vue-hot-reload-api").rerender("data-v-19b7a970", module.exports)
+  }
+}
+
+/***/ }),
+
+/***/ 7:
+/***/ (function(module, exports, __webpack_require__) {
+
+var Component = __webpack_require__(0)(
+  /* script */
+  __webpack_require__(9),
+  /* template */
+  __webpack_require__(13),
+  /* scopeId */
+  null,
+  /* cssModules */
+  null
+)
+Component.options.__file = "D:\\WINDOWS\\GD2\\web\\_node_dev\\td_git_vueCheckToggle1\\src\\vue-components\\Home.vue"
+if (Component.esModule && Object.keys(Component.esModule).some(function (key) {return key !== "default" && key !== "__esModule"})) {console.error("named exports are not supported in *.vue files.")}
+if (Component.options.functional) {console.error("[vue-loader] Home.vue: functional components are not supported with templates, they should use render functions.")}
+
+/* hot reload */
+if (false) {(function () {
+  var hotAPI = require("vue-hot-reload-api")
+  hotAPI.install(require("vue"), false)
+  if (!hotAPI.compatible) return
+  module.hot.accept()
+  if (!module.hot.data) {
+    hotAPI.createRecord("data-v-19b7a970", Component.options)
+  } else {
+    hotAPI.reload("data-v-19b7a970", Component.options)
+  }
+})()}
+
+module.exports = Component.exports
+
+
+/***/ }),
+
+/***/ 9:
+/***/ (function(module, __webpack_exports__, __webpack_require__) {
+
+"use strict";
+Object.defineProperty(__webpack_exports__, "__esModule", { value: true });
+/* harmony import */ var __WEBPACK_IMPORTED_MODULE_0__js_componentinjector_js__ = __webpack_require__(10);
+//
+//
+//
+//
+//
+//
+//
+//
+//
+//
+//
+//
+//
+//
+//
+//
+//
+//
+//
+//
+//
+//
+//
+//
+//
+//
+
+;
+/* harmony default export */ __webpack_exports__["default"] = ({
+  data: function data() {
+    return {};
+  },
+
+  mounted: function mounted() {
+    this.mountCheckToggle1();
+  },
+  methods: {
+    mountCheckToggle1: function mountCheckToggle1() {
+      var CheckToggle1 = function CheckToggle1(resolve) {
+        __webpack_require__.e/* require.ensure */(1).then((function () {
+          resolve(__webpack_require__(8));
+        }).bind(null, __webpack_require__)).catch(__webpack_require__.oe);
+      };
+      __WEBPACK_IMPORTED_MODULE_0__js_componentinjector_js__["a" /* inject */]("#vcCheckToggle1_mount", CheckToggle1);
+    }
+  }
+});
+
+/***/ })
+
+});
--- src/vue-components/Home.vue
+++ src/vue-components/Home.vue
@@ -1,9 +1,27 @@
 <template>
   <div>
     <h2 class="item-id">vueCheckToggle1-01</h2>
-    <!-- vueCheckToggle1-01 mount-->
-    <div id="vcCheckToggle1_mount"></div>
-    <!-- end vueCheckToggle1-01 mount-->
+    <div class="row">    
+      <div class="col-sm-2">
+        <div id="vcCheckToggle1_mount">
+        </div>
+      </div>
+      <div class="col-sm-2">
+        2
+      </div>
+      <div class="col-sm-2">
+        3
+      </div>
+      <div class="col-sm-2">
+        4
+      </div>
+      <div class="col-sm-2">
+        5
+      </div>
+      <div class="col-sm-2">
+        6
+      </div>
+    </div>  
   </div>
 </template>
 <script>
