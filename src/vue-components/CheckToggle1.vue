9cd5ca4f33598a3124123099c22ee8484e568252
administrator <liquidnuker@users.noreply.github.com>
2017-07-18 11:42:06 -0700

    added checkAll + unCheckAll function

--- src/vue-components/CheckToggle1.vue
+++ src/vue-components/CheckToggle1.vue
@@ -5,7 +5,7 @@
       <template v-for="i in items">
       <span class="checkToggle1-01">
         <input type="checkbox" v-bind:id="i"
-        v-model="selectedItems" v-bind:value="i" />
+         v-model="selectedItems" v-bind:value="i" />
         <label v-bind:for="i" class="checkToggle1-01_bg">
           <span class="checkToggle1-01_switch">
             &nbsp;
@@ -17,6 +17,8 @@
       </span>
       </template>
       <!-- end checkToggle1-01 -->
+      <button :disabled="checkAllButton" v-on:click="checkAll()">checkAll</button>
+      <button v-on:click="unCheckAll()">unCheckAll</button>
       <p>Selected: {{ selectedItems }}</p>
     </div>  
   </div>
@@ -29,7 +31,28 @@ export default {
         "item1",
         "item2"
       ],
-      selectedItems: []
+      selectedItems: [],
+      checkAllButton: false
+    }
+  },
+  mounted: function() {
+    console.log("mounted");
+    this.checkAll();
+  },
+  methods: {
+    checkAll: function() {
+      let items = this.items;
+      let selectedItems = this.selectedItems;
+
+      selectedItems = [];
+      for (let i in items) {
+        selectedItems.push(items[i]);
+      }   
+      this.selectedItems = selectedItems;
+
+    },
+    unCheckAll: function() {
+      this.selectedItems = [];
     }
   }
 }
